60dca7b25a61db162673a5510bc96b062af0f98e
Christian Versloot <mail@christianversloot.nl>
2019-12-02 20:11:14 +0100

    Keract based activations

---
+++ keract_activations.py
@@ -0,0 +1,84 @@
+'''
+  Visualize layer activations of a Keras CNN with Keract
+'''
+
+# =============================================
+# Model to be visualized
+# =============================================
+import keras
+from keras.datasets import mnist
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Conv2D, MaxPooling2D
+from keras import backend as K
+from keras import activations
+
+# Model configuration
+img_width, img_height = 28, 28
+batch_size = 250
+no_epochs = 25
+no_classes = 10
+validation_split = 0.2
+verbosity = 1
+
+# Load MNIST dataset
+(input_train, target_train), (input_test, target_test) = mnist.load_data()
+
+# Reshape data based on channels first / channels last strategy.
+# This is dependent on whether you use TF, Theano or CNTK as backend.
+# Source: https://github.com/keras-team/keras/blob/master/examples/mnist_cnn.py
+if K.image_data_format() == 'channels_first':
+    input_train = input_train.reshape(input_train.shape[0], 1, img_width, img_height)
+    input_test = input_test.reshape(input_test.shape[0], 1, img_width, img_height)
+    input_shape = (1, img_width, img_height)
+else:
+    input_train = input_train.reshape(input_train.shape[0], img_width, img_height, 1)
+    input_test = input_test.reshape(input_test.shape[0], img_width, img_height, 1)
+    input_shape = (img_width, img_height, 1)
+
+# Parse numbers as floats
+input_train = input_train.astype('float32')
+input_test = input_test.astype('float32')
+
+# Normalize data
+input_train = input_train / 255
+input_test = input_test / 255
+
+# Convert target vectors to categorical targets
+target_train = keras.utils.to_categorical(target_train, no_classes)
+target_test = keras.utils.to_categorical(target_test, no_classes)
+
+# Create the model
+model = Sequential()
+model.add(Conv2D(6, kernel_size=(5, 5), activation='relu', input_shape=input_shape))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Conv2D(10, kernel_size=(5, 5), activation='relu'))
+model.add(Flatten())
+model.add(Dense(256, activation='relu'))
+model.add(Dense(no_classes, activation='softmax'))
+
+# Compile the model
+model.compile(loss=keras.losses.categorical_crossentropy,
+              optimizer=keras.optimizers.Adam(),
+              metrics=['accuracy'])
+
+# Fit data to model
+model.fit(input_train, target_train,
+          batch_size=batch_size,
+          epochs=no_epochs,
+          verbose=verbosity,
+          validation_split=validation_split)
+
+# Generate generalization metrics
+score = model.evaluate(input_test, target_test, verbose=0)
+print(f'Test loss: {score[0]} / Test accuracy: {score[1]}')
+
+# =============================================
+# Keract visualizations
+# =============================================
+from keract import get_activations, display_activations, display_heatmaps
+keract_inputs = input_test[:1]
+keract_targets = target_test[:1]
+activations = get_activations(model, keract_inputs)
+display_activations(activations, cmap="gray", save=False)
+display_heatmaps(activations, keract_inputs, save=False)
